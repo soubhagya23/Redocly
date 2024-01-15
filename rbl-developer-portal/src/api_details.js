3a56013f8a3b24c8dd1cbf76761d81d3b42ad5a0
soubhagya23 <ranjansoubhagya.master@gmail.com>
2024-01-15 20:33:06 +0530

    v3

--- rbl-developer-portal/src/api_details.js
+++ rbl-developer-portal/src/api_details.js
@@ -1,46 +1,80 @@
 import React from "react";
 import { RedocStandalone } from "redoc";
-
 import Swagger from "./swagger.json";
 
 export default function APIDetails(props) {
   return (
     <>
-      {/*  <Header /> */}
+      {/* <Header />  */}
       <div id="redoc-container">
         <RedocStandalone
-          onComplete={() => console.log("Redoc rendering complete")}
-          onHashChange={(hash) => console.log("Hash changed:", hash)}
           specUrl={Swagger}
           options={{
             disableSearch: true,
-
+            suppressWarnings: true,
+            requiredPropsFirst: true,
+            sortPropsAlphabetically: true,
+            pathInMiddlePanel: true,
             theme: {
-              colors: { primary: { main: "#dd5522" } },
+              colors: {
+                text: {
+                  primary: "#424242",
+                  secondary: "#424242",
+                },
+                tonalOffset: 0.2,
+              },
+
+              typography: {
+                fontSize: "14px",
+                lineHeight: "1.5em",
+                fontWeightRegular: "400",
+                fontWeightBold: "600",
+                fontWeightLight: "300",
+
+                headings: {
+                  fontWeight: "600",
+                },
+              },
+              schemaView: {
+                linesColor: '#DFC350',
+                typeNameColor: '#DFC350',
+                typeTitleColor: '#DFC350',
+                requireLabelColor: '#DFC350',
+                nestingSpacing: '#DFC350'
+            },
               sidebar: {
                 activeBgColor: "red",
-                activeTextColor: "green",
+                activeTextColor: "blue",
+                backgroundColor: "#ededed",
               },
 
               rightPanel: {
-                backgroundColor: "white",
+                backgroundColor: "#186FAF",
                 width: "30%",
-                textColor: "white",
+                textColor: "#ffffff",
+                
               },
+            
               codeBlock: {
-                backgroundColor: "black",
+                fontSize: "14px",
+                color:'green',
+                backgroundColor: "#000000",
                 borderRadius: "20px",
                 tokens: {
-                  error: {
-                    color:'green'
-                  }
-                }
-              
+                  color: '#a0fbaa',
+                },
+                codeSample:{
+                  color: '#a0fbaa',
+                },
+                text: {
+                  secondary: {
+                    color: "black",
+                  },
+                },
               },
               components: {
-                borderRadius:20
+                borderRadius: 20,
               },
-           
             },
           }}
         />
